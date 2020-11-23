4901f12b1f860cc8ceeadbd9ae533fc513860ba6
Raúl Kripalani <raul@protocol.ai>
2020-11-23 23:01:13 +0000

    cgroups.

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/raulk/trampoline
 go 1.15
 
 require (
-	github.com/testground/sdk-go v0.2.6
-	github.com/testground/sdk-go/virtual/1 v0.0.0
-	github.com/testground/sdk-go/virtual/2 v0.0.0
+	github.com/containerd/cgroups v0.0.0-20201119153540-4cbc285b3327
+	github.com/opencontainers/runtime-spec v1.0.2
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 )
 
 replace github.com/testground/sdk-go/virtual/1 => github.com/testground/sdk-go v0.2.3
--- main.go
+++ main.go
@@ -1,64 +1,120 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"runtime"
 	"strconv"
+
+	"github.com/containerd/cgroups"
+	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
 var data [][]byte // keep byte slabs
 
 func main() {
-	http.HandleFunc("/add", add)
-	http.HandleFunc("/release", release)
+	interactive := flag.Bool("interactive", false, "start in interactive HTTP mode")
+	limit := flag.Int64("limit", 32, "memory limit in MiB")
+
+	// delete the cgroup if it exists.
+	if cgroup, err := cgroups.Load(cgroups.V1, cgroups.StaticPath("/trampoline")); err == nil {
+		log.Printf("prexisting cgroup deleted")
+		_ = cgroup.Delete()
+	}
+
+	// create the cgroup.
+	cgroup, err := cgroups.New(cgroups.V1, cgroups.StaticPath("/trampoline"), &specs.LinuxResources{
+		Memory: &specs.LinuxMemory{
+			Limit: limit,
+			Swap:  limit,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer cgroup.Delete()
+
+	log.Printf("cgroup created: trampoline")
+
+	if err := cgroup.Add(cgroups.Process{Pid: os.Getpid()}); err != nil {
+		panic(fmt.Sprintf("failed to add process to group: %s", err))
+	}
+
+	log.Printf("process added to cgroup")
+
+	if *interactive {
+		interactiveMode()
+		return
+	}
+
+}
+
+func interactiveMode() {
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		bytes, err := parseBytes(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		add(bytes)
+
+		_, _ = fmt.Fprintln(w, "added: ", bytes)
+		_, _ = fmt.Fprintln(w)
+		writeMemStats(w)
+	})
+
+	http.HandleFunc("/rel", func(w http.ResponseWriter, r *http.Request) {
+		bytes, err := parseBytes(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		released, not := release(bytes)
+
+		_, _ = fmt.Fprintln(w, "released: ", released)
+		_, _ = fmt.Fprintln(w, "not released: ", not)
+		_, _ = fmt.Fprintln(w)
+		writeMemStats(w)
+	})
+
 	http.HandleFunc("/gc", gc)
 	http.HandleFunc("/stats", stats)
 	http.HandleFunc("/reset", reset)
 
-	fmt.Println("started")
+	fmt.Println("http endpoint started")
 
 	_ = http.ListenAndServe("0.0.0.0:1112", http.DefaultServeMux)
+
+	return
 }
 
-func add(w http.ResponseWriter, r *http.Request) {
+func parseBytes(r *http.Request) (int, error) {
 	bytesStr, ok := r.URL.Query()["bytes"]
 	if !ok || len(bytesStr) == 0 {
-		http.Error(w, "please specify how many bytes to allocate in the 'bytes' query parameter", http.StatusBadRequest)
-		return
+		return -1, errors.New("invalid 'bytes' query parameter")
 	}
-
 	bytes, err := strconv.Atoi(bytesStr[0])
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+		return -1, err
 	}
+	return bytes, nil
+}
 
+func add(bytes int) {
 	m := make([]byte, bytes)
 	for j := range m {
 		m[j] = 1
 	}
 	data = append(data, m)
-
-	_, _ = fmt.Fprintln(w, "added: ", bytes)
-	_, _ = fmt.Fprintln(w)
-
-	writeMemStats(w)
 }
 
-func release(w http.ResponseWriter, r *http.Request) {
-	bytesStr, ok := r.URL.Query()["bytes"]
-	if !ok || len(bytesStr) == 0 {
-		http.Error(w, "please specify how many bytes to allocate in the 'bytes' query parameter", http.StatusBadRequest)
-		return
-	}
-
-	bytes, err := strconv.Atoi(bytesStr[0])
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
+func release(bytes int) (released, notReleased int) {
 	rem := bytes
 	for i := 0; i < len(data) && rem > 0; i++ {
 		head := data[i]
@@ -72,12 +128,7 @@ func release(w http.ResponseWriter, r *http.Request) {
 			rem = 0
 		}
 	}
-
-	_, _ = fmt.Fprintln(w, "released: ", bytes-rem)
-	_, _ = fmt.Fprintln(w, "not released: ", rem)
-	_, _ = fmt.Fprintln(w)
-
-	writeMemStats(w)
+	return bytes - rem, rem
 }
 
 func gc(w http.ResponseWriter, r *http.Request) {
